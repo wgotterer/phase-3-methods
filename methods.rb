eb8c0f4f9120a495dfea49fb21801ed83df37e59
William Gotterer <william.gotterer@gmail.com>
2021-10-30 14:52:52 -0400

    Done.

--- methods.rb
+++ methods.rb
@@ -2,6 +2,27 @@ def greet_programmer
     puts "Hello, programmer!"
 end
 
+greet_programmer
+
 def greet name
     puts "Hello #{name} !"
 end
+
+def greet_with_default (name = "programmer")
+    puts("Hello, #{name}!")
+end
+
+def add (num1, num2)
+    num1 + num2
+end
+
+def halve (number)
+    if number.class != String
+        return nil
+    end
+    number / 2
+end
+
+halve(3)
+
+
